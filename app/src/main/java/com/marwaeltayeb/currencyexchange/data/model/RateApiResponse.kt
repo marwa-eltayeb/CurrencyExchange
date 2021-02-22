7991774b5996184d3be692081525ea6f57a0faec
Marwa-Said <marwa_eltayeb@yahoo.com>
2021-02-22 21:20:13 +0200

    Use Map to get sorted currency codes

--- app/src/main/java/com/marwaeltayeb/currencyexchange/data/model/RateApiResponse.kt
+++ app/src/main/java/com/marwaeltayeb/currencyexchange/data/model/RateApiResponse.kt
@@ -1,12 +1,10 @@
 package com.marwaeltayeb.currencyexchange.data.model
 
-
 import com.google.gson.annotations.SerializedName
-import java.util.*
 
 data class RateApiResponse(
     @SerializedName("rates")
-    val rates: Hashtable<String, Double>,
+    val rates: Map<String, Double>,
     @SerializedName("base")
     val base: String,
     @SerializedName("date")
